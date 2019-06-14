4e8faefd927190824a56fac56d32f4c6ea665f84
Franco Horita <franco.horita@gmail.com>
2019-06-14 16:06:47 -0300

    Testing Qualifier

--- src/main/java/com/solstice/spring/basics/springin10steps/BinarySearchImpl.java
+++ src/main/java/com/solstice/spring/basics/springin10steps/BinarySearchImpl.java
@@ -1,17 +1,21 @@
 package com.solstice.spring.basics.springin10steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
-    private SortAlgorithm bubbleSortAlgorithm;
+    @Qualifier("bubble")
+    private SortAlgorithm sortAlgorithm;
 
     public int binarySearch(int[] numbers, int numberToSearch){
 
-        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
+
+        int[] sortedNumbers = sortAlgorithm.sort(numbers);
 
         return 3;
 
--- src/main/java/com/solstice/spring/basics/springin10steps/QuickSortAlgorithm.java
+++ src/main/java/com/solstice/spring/basics/springin10steps/QuickSortAlgorithm.java
@@ -1,9 +1,11 @@
 package com.solstice.spring.basics.springin10steps;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Primary -- Primary notation overrides the Autowire by name.
+@Qualifier("quick")
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers){
