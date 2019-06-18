94805de87389b9c214cc34c457c17aa3a8a39ef3
Franco Horita <franco.horita@gmail.com>
2019-06-18 14:19:57 -0300

    Lifecycle of a Bean

--- src/main/java/com/solstice/spring/basics/springin10steps/basic/BinarySearchImpl.java
+++ src/main/java/com/solstice/spring/basics/springin10steps/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.solstice.spring.basics.springin10steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //@PreDestroy works if the bean is Singleton
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("bubble")
@@ -22,6 +29,16 @@ public class BinarySearchImpl {
 
         return 3;
 
+    }
+
+    @PostConstruct
+    private void postConstruct(){
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    private void preDestroy(){
+        logger.info("preDestroy");
     }
 
 }
