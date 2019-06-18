386d6a1b09dde8be49161631bfa50fa1dfefb1aa
Franco Horita <franco.horita@gmail.com>
2019-06-18 10:19:02 -0300

    Rename Test Basic Application

--- src/test/java/com/solstice/spring/basics/springin10steps/SpringIn10StepsBasicApplicationTests.java
+++ src/test/java/com/solstice/spring/basics/springin10steps/SpringIn10StepsBasicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringIn10StepsApplicationTests {
+public class SpringIn10StepsBasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
