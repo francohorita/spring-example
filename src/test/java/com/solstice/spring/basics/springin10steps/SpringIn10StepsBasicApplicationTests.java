702a76acda20745d39b629419a3c80ddce06c2f7
Franco Horita <franco.horita@gmail.com>
2019-06-21 14:20:48 -0300

    Adding simple unit test

--- src/test/java/com/solstice/spring/basics/springin10steps/SpringIn10StepsBasicApplicationTests.java
+++ src/test/java/com/solstice/spring/basics/springin10steps/SpringIn10StepsBasicApplicationTests.java
@@ -1,16 +1,28 @@
 package com.solstice.spring.basics.springin10steps;
 
+import com.solstice.spring.basics.springin10steps.basic.BinarySearchImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes=SpringIn10StepsPropertiesApplication.class)
 public class SpringIn10StepsBasicApplicationTests {
 
+	@Autowired
+	BinarySearchImpl binarySearch;
+
 	@Test
-	public void contextLoads() {
+	public void testBasicScenario() {
+		int result = binarySearch.binarySearch(new int[] {}, 5);
+		assertEquals(3, result);
+
 	}
 
 }
